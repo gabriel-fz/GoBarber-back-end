1ffd53e73353e53dbf2266748de93877c7a1d3c5
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-14 18:25:04 -0300

    arquivos completos de cadastro e autenticaçao de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello rocketseat' });
-});
+// rota de criação de usuário
+routes.post('/users', UserController.store);
+
+// rota de autenticação de usuário
+routes.post('/sessions', SessionController.store);
+
+// middleware global de autenticação
+routes.use(authMiddleware);
+
+// rota de alteração de usuário
+routes.put('/users', UserController.update);
 
 export default routes;
