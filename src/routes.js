2ddbe28a71227223f811504ce69660ccf231ab8b
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-19 16:12:41 -0300

    modulo 1 concluido

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // rota de criação de usuário
 routes.post('/users', UserController.store);
@@ -18,5 +22,8 @@ routes.use(authMiddleware);
 
 // rota de alteração de usuário
 routes.put('/users', UserController.update);
+
+// rote de upload de imagem
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
