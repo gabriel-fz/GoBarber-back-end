e70d82ac35ca026f41816c7a164b8e73de651fb9
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-19 20:57:52 -0300

    modulo 2 concluido

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,7 +26,19 @@ routes.use(authMiddleware);
 // rota de alteração de usuário
 routes.put('/users', UserController.update);
 
-// rote de upload de imagem
+// rota de listagem de prestadores de serviço
+routes.get('/providers', ProviderController.index);
+
+// rota de listagem de agendamento
+routes.get('/appointments', AppointmentController.index);
+
+// rota de agendamento
+routes.post('/appointments', AppointmentController.store);
+
+// rota de listagem de agendamentos para o prestador
+routes.get('/schedule', ScheduleController.index);
+
+// rota de upload de imagem
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
