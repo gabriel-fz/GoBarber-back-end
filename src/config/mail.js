66a04fb906050922040e049dbac346700258b29e
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-20 18:58:08 -0300

    modulo 5 concluido

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '59dad3eda01fcf',
-    pass: 'fdb50c05144f68',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://f56600b564cf4e4ab85f8d03c90673f2@sentry.io/2718010',
+};
